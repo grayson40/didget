679e4acdd2724c9447076e1f8c640d8eb2b1ac26
Grayson Crozier <graysoncrozier40@gmail.com>
2022-09-13 20:55:22 -0400

    Cleanup master

--- main.js
+++
@@ -1,7 +0,0 @@
-console.log('hello grayson');
-console.log('hello chance');
-console.log('hello justin');
-console.log('hello Didget Team');
-console.log('hello Chance1');
-console.log('hello justin');
-console.log('yo justin, where my brackets at?');
