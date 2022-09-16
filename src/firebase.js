96c1da9ff3b8c575752bbc6553bc9def5b5b128d
Grayson Crozier <graysoncrozier40@gmail.com>
2022-09-16 17:02:21 -0400

    Update firrebase initialization

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: "AIzaSyDP5a0qE3cu7RiIRFlq4CU_s2UFTLRj5SU",
   authDomain: "didget-development.firebaseapp.com",
   projectId: "didget-development",
   storageBucket: "didget-development.appspot.com",
   messagingSenderId: "730253438625",
   appId: "1:730253438625:web:b152fcc400f260f4544ae0"
-};
+});
 
 // Initialize Firebase
 export const auth = app.auth();
