1369fb65688f68ef13c84fef331d3ebae9e650d2
Grayson Crozier <graysoncrozier40@gmail.com>
2022-09-20 23:42:55 -0400

    Add login and sign up routing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Login() {
     const emailRef = useRef()
@@ -9,6 +9,7 @@ export default function Login() {
     const { login, currentUser } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -17,6 +18,7 @@ export default function Login() {
             setError('')
             setLoading(true)
             await login(emailRef.current.value, passwordRef.current.value)
+            navigate('/')
         }
         catch {
             setError('Failed to Log In')
@@ -29,7 +31,6 @@ export default function Login() {
         <Card>
             <Card.Body>
                 <h2 className="text-center mb-4">Log In</h2>
-                {currentUser && currentUser.email}
                 {error && <Alert variant="danger">{error}</Alert>}
                 {/*Create Email, Password, and Password-Confirm Fields on page*/}
                 <Form onSubmit={handleSubmit}>
