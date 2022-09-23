2f99024cc9f29b72179ddf6a7d1bb47545b9ef11
Jeanne Moore <mooresea910@gmail.com>
2022-09-23 17:56:34 -0400

    Finished Top Bar Skeleton

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -9,26 +9,38 @@
 //              The signout option should logout the user and route back to signout
 //              All pages except for Signout and Login pages should be set as private routes
 
-import React, { useRef, useState } from 'react'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
-import { formatError, useAuth } from '../contexts/AuthContext'
-import { Link, useNavigate } from 'react-router-dom'
+import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 export default function TopBar() {
   return (
     //  Create a nav bar which is situated at the top of the given workspace
     <Navbar fixed="top">
-      <Container>
-        <Navbar.Brand href="#home">This is a title!</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            I can still hear you saying: <a href="#login">you</a> would never break the chain
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
+        <Container fluid>
+            {/* Top Bar should consist of a leftbound drop down */}
+            {/* To be replaced with a hamburger dropdown button */}
+            <DropdownButton id="dropdown-basic-button" title="Go to...">
+                <Dropdown.Item href="#/action-1">Tasking</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Budgeting</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Debt Tracking</Dropdown.Item>
+            </DropdownButton>
+            
+            {/* Top Bar should also consist of an app title */}
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-center">
+                <Navbar.Brand href="#home">Didget</Navbar.Brand>
+            </Navbar.Collapse>
+
+            {/* Top Bar should finally consist of a right drop down button */}
+            {/* Could be replaced with a profile icon dropdownbutton setup */}
+            <DropdownButton id="dropdown-basic-button" title="Profile">
+                <Dropdown.Item href="#/action-4">Settings</Dropdown.Item>
+                <Dropdown.Item href="#/action-5">Signout</Dropdown.Item>
+            </DropdownButton>
+        </Container>
     </Navbar>
   );
 }
