d1bd14988d7f39a44fb05f15a9ff7d8021068424
Chance Rupnow <chancerupnow@gmail.com>
2022-09-25 16:36:40 -0400

    Added card functionality and links to App & Dash

---
+++ src/components/Cards.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import { Button, Collapse } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+
+export default function Cards() {
+
+    const [open1, setOpen1] = useState(true)
+    const [open2, setOpen2] = useState(true)
+    const [open3, setOpen3] = useState(true)
+
+  return (
+    <div>
+        <Container fluid style = {{ height: '600px'}}>
+            <Card>
+                <Button
+                    onClick={() => setOpen1(!open1)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open1}
+                >
+                    Tasks
+                </Button>
+                <Collapse in={open1}>
+                    <div id="example-collapse-text">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+
+                    </div>
+                </Collapse>
+            </Card>
+            <Card>
+                <Button
+                    onClick={() => setOpen2(!open2)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open2}
+                >
+                    Budgeting
+                </Button>
+                <Collapse in={open2}>
+                    <div id="example-collapse-text">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    </div>
+                </Collapse>
+            </Card>
+            <Card>
+                <Button
+                    onClick={() => setOpen3(!open3)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open3}
+                >
+                    Notes
+                </Button>
+                <Collapse in={open3}>
+                    <div id="example-collapse-text">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    </div>
+                </Collapse>
+            </Card>
+        </Container>
+    </div>
+  );
+}
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import TopBar from './TopBar';
+import Cards from './Cards';
 
 export default function Dashboard() {
   return (
     <Container>
       <TopBar/>
+      <Cards/>
     </Container>
   )
 }
