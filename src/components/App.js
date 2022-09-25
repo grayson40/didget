6b0d9881da21c2099372c2495773b228cca07038
Chance Rupnow <chancerupnow@gmail.com>
2022-09-25 17:24:04 -0400

    Removed ./cards pathing from app.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Dashboard from './Dashboard'
 import Login from './Login'
 import PrivateRoute from './PrivateRoute'
-import Cards from './Cards'
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
               <Route exact path="/" element={<PrivateRoute> <Dashboard/> </PrivateRoute>}/>
               <Route path = "/signup" element={<Signup/>}/>
               <Route path = "/login" element={<Login/>}/>
-              <Route path = "/cards" element={<Cards/>}/>
             </Routes>
           </AuthProvider>
         </Router>
