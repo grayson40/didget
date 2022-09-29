673cf2c6d55206bb6f4cf2aa8a2524f75cbb4076
Grayson Crozier <graysoncrozier40@gmail.com>
2022-09-29 18:15:34 -0400

    change

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 import { Button, Collapse } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 
-// change 1
+// change
 export default function Cards() {
 
     const [open1, setOpen1] = useState(true)
