df0e1d7814fa2d1d3c94a0e1ef214904611fd4bd
Chance Rupnow <chancerupnow@gmail.com>
2022-09-29 19:23:47 -0400

    temporary stack structure created

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import TopBar from './TopBar';
+import Stack from 'react-bootstrap/Stack';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Notes() {
     return (
         <Container>
             <TopBar name='Notes'/>
+            <Container fluid style = {{ height: '600px'}}>
+                <Stack gap={3}>
+                    <div className="bg-light border">
+                        <Row>
+                            <Col sm={8}>Name1</Col>
+                            <Col sm={4}>Creation1</Col>
+                        </Row>
+                    </div>
+                    <div className="bg-light border">
+                        <Row>
+                            <Col sm={8}>Name2</Col>
+                            <Col sm={4}>Creation2</Col>
+                        </Row>
+                    </div>
+                    <div className="bg-light border">
+                        <Row>
+                            <Col sm={8}>Name3</Col>
+                            <Col sm={4}>Creation3</Col>
+                        </Row>
+                    </div>
+                </Stack>
+            </Container>
         </Container>
     )
 }
