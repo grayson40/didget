e2223fb852070fc671303a105e159890f9a6806e
Jeanne Moore <mooresea910@gmail.com>
2022-09-29 18:32:07 -0400

    Created HTML Datepicker, not much functionality yet

--- src/components/DateBar.js
+++ src/components/DateBar.js
@@ -1,24 +1,16 @@
 import React from 'react'
-import { Button, Container, Navbar, Dropdown, DropdownButton, Nav, NavDropdown } from 'react-bootstrap';
+import { Form, Button, Container, Navbar, Dropdown, DropdownButton, Nav, NavDropdown } from 'react-bootstrap';
 import './styles.css'
 
 export default function DateBar(){
     //  Create a variable that tracks the date currently being looked at
-    let today = new Date().toLocaleDateString();
+    let today = new Date;
 
     return(
         <Nav class = "navbar navbar-toggleable-sm bg-faded navbar-light fixed-top fixed-top-2">
-            <div class = "container-fluid d-flex p-2 justify-content-center">
-                {/* Create a dropdown menu that will hold a container and a datepicker inside */}
-                <NavDropdown title = {today}>
-                    {/* Content inside dropdown is temporary */}
-                    <NavDropdown.Item>
-                        Item A
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                        Item B
-                    </NavDropdown.Item>
-                </NavDropdown>
+            <div class = "container-fluid p-2 justify-content-center">
+                {/* Create a datepicker on the datebar */}
+                <input type="date" id={today.toLocaleDateString()} name="date-field"/>
             </div>
         </Nav>
     );
