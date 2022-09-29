9fa403a4c3d98090ec4c8ed7b2c06408e9a5a863
Chance Rupnow <chancerupnow@gmail.com>
2022-09-29 18:01:39 -0400

    test commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import { Button, Collapse } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 
+// change
 export default function Cards() {
 
     const [open1, setOpen1] = useState(true)
