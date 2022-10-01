90f7f332651cdb3f4de7ecc6d01f50c880c20d43
Jeanne Moore <mooresea910@gmail.com>
2022-10-01 16:18:05 -0400

    topbar changes pushed into a different branch, my branch topbar reverted

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -10,10 +10,10 @@
 //              All pages except for Signout and Login pages should be set as private routes
 
 import React from 'react';
+import {  Navbar, Dropdown, DropdownButton } from 'react-bootstrap';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
-import { FaBars } from 'react-icons/fa';
-import { HiCog } from 'react-icons/hi'
+import { FaGrinSquint, FaBars } from 'react-icons/fa';
 import './styles.css';
 
 
@@ -30,29 +30,38 @@ export default function TopBar({ name }) {
 
   //  Create title constants for the navbar dropdowns
   const quickAccTitle = (<FaBars glyph="star"> Dropdown </FaBars>);
-  const profSettTitle = (<HiCog glyph="star"> Dropdown </HiCog>);
 
   return (
-      <nav>
-        <div class="dropdown fixed-top">
-          <div class="quickaccess">
-            <button><a href='#'>{quickAccTitle}</a></button>
-            <ul>
-              <li><a href='#'>Home</a></li>
-              <li><a href='#'>Schedule</a></li>
-              <li><a href='#'>Budget</a></li>
-              <li><a href='#'>Notes</a></li>
-            </ul>
-          </div>
-          <label>{ name }</label>
-          <div class="profile">
-          <button><a href='#'>{profSettTitle}</a></button>
-            <ul>
-              <li><a href='#'>Settings</a></li>
-              <li><a href='#'>Sign Out</a></li>
-            </ul>
-          </div>
+
+    //  Create a nav bar which is situated at the top of the given workspace
+    <Navbar bg="dark" variant="dark" fixed="top">
+        <div class = "container-fluid d-flex justify-content-start">
+
+            {/* Top Bar should consist of a leftbound drop down */}
+            {/* To be replaced with a hamburger dropdown button */}
+            <DropdownButton class="dropdown-toggle" id="dropdown-basic-button" title={quickAccTitle}>
+              <Dropdown.Item href="/">Home</Dropdown.Item>
+              <Dropdown.Item href="/schedule">Schedule</Dropdown.Item>
+              <Dropdown.Item href="/budget">Budget</Dropdown.Item>
+              <Dropdown.Item href="/notes">Notes</Dropdown.Item>
+          </DropdownButton>
         </div>
-      </nav>
+        <div class = "container-fluid d-flex justify-content-center">
+          {/* Top Bar should also consist of an app title */}
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-center">
+              <Navbar.Brand href="/">{ name }</Navbar.Brand>
+          </Navbar.Collapse>
+        </div>
+        <div class = "container-fluid d-flex justify-content-end">
+
+          {/* Top Bar should finally consist of a right drop down button */}
+          {/* Could be replaced with a profile icon dropdownbutton setup */}
+          <DropdownButton icon={<FaGrinSquint />} title={"Profile"} id="dropdown-basic-button">
+            <Dropdown.Item href="/settings">Settings</Dropdown.Item>
+            <Dropdown.Item onClick={logOut}>Sign Out</Dropdown.Item>
+          </DropdownButton>
+        </div>
+    </Navbar>
   );
 }
