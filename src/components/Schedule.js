0e96b693da8b8c8c7b5f88626e5a3703f22f33df
Justin <jdk.florida@gmail.com>
2022-10-04 19:27:27 -0400

    Added Dropdown to Classes for Scheduler

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import TopBar from './TopBar'
 import Card from 'react-bootstrap/Card'
-import { Button, Collapse, Row, Col} from 'react-bootstrap'
+import { Button, Collapse, Row, Col, Dropdown, DropdownButton} from 'react-bootstrap'
+import { FaEllipsisH } from 'react-icons/fa'
 
 export default function Schedule() {
 
@@ -10,11 +11,21 @@ export default function Schedule() {
 
   return (
     <Container>
-      <TopBar name='Schedule'/>
+      <TopBar name='Didget - Schedule'/>
       {/*Create a card for each class, likely to be replaced with its own component*/}
       <Card className='mb-4'>
         {/*Card Header is the name of the class*/}
-        <Card.Header as="h5">Class 1 Name</Card.Header>
+        <Card.Header as="h5">
+            <Row>
+                <Col sm={8}>Class 1 Name</Col>
+                <Col xs={0}>
+                    <DropdownButton id="dropdown-basic-button" title={<FaEllipsisH/>} style={{textAlign: "right"}}>
+                        <Dropdown.Item href="#/action-1">Edit</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Delete</Dropdown.Item>
+                    </DropdownButton>
+                </Col>
+            </Row>
+        </Card.Header>
             <Card.Body>
                 {/*Card title is the time of the class and days when the class meets*/}
                 <Card.Title>M/W/F 10:00 am - 11:30 am</Card.Title>
@@ -58,7 +69,7 @@ export default function Schedule() {
                 <Button variant="primary">Tasks</Button>
             </Card.Body>
         </Card>
-        
+
     </Container>
     
   )
