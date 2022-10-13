8e793b997748deb003c98c5324237d9b13c2403f
Justin <jdk.florida@gmail.com>
2022-10-13 17:07:20 -0400

    how to graph? ... sad

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -8,7 +8,8 @@ export default function Expenses() {
     return (
         <Container fixed="top" fluid style={{ width: '450px', marginTop: "5%" }}>
             <TopBar name="Expenses" />
-            
+            {/*GRAPH PLACEHOLDER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+            <Image src='https://www.tibco.com/sites/tibco/files/media_entity/2022-01/doughnut-chart-example.svg' style={{ height: "340px"}}/>
             <Card style={{ width: '450px', textAlign: "Center" }} className="mb-2">
                 <Card.Header>
                     Expenses
