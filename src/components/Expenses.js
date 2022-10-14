71c992962ffa0efd9a58e66a97390aafe24dd01b
Justin <jdk.florida@gmail.com>
2022-10-13 20:21:36 -0400

    Modify Expenses Page

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Container, Card, ListGroup, Row, Col, Image } from 'react-bootstrap'
+import { Container, Card, Row, Col, Image } from 'react-bootstrap'
+import Fab from '@mui/material/Fab'
+import { FaPlus } from 'react-icons/fa'
 import TopBar from './TopBar'
 
 
@@ -9,18 +11,18 @@ export default function Expenses() {
         <Container fixed="top" fluid style={{ width: '450px', marginTop: "5%" }}>
             <TopBar name="Expenses" />
             {/*GRAPH PLACEHOLDER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
-            <Image src='https://www.tibco.com/sites/tibco/files/media_entity/2022-01/doughnut-chart-example.svg' style={{ height: "340px"}}/>
+            <Image src='https://www.tibco.com/sites/tibco/files/media_entity/2022-01/doughnut-chart-example.svg' style={{ height: "340px" }} />
             <Card style={{ width: '450px', textAlign: "Center" }} className="mb-2">
                 <Card.Header>
                     Expenses
                 </Card.Header>
-                <Card.Body>
+                <Card.Header>
                     <Row>
                         <Col sm={4} className="border-end">Name</Col>
                         <Col sm={4} className="border-end">Total</Col>
                         <Col sm={4}>Date</Col>
                     </Row>
-                </Card.Body>
+                </Card.Header>
             </Card>
             <Card style={{ width: '450px', textAlign: "Center" }} className="mb-2">
                 <Card.Body>
@@ -49,6 +51,13 @@ export default function Expenses() {
                     </Row>
                 </Card.Body>
             </Card>
+
+            <div style={{ position: "fixed", bottom: "20px", justifyContent: 'flex-end', display: 'flex' }}>
+                <Fab size={"80px"} color="primary">
+                    <FaPlus size={"30px"} />
+                </Fab>
+            </div>
         </Container>
+
     )
 }
