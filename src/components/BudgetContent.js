33fbf802c79aea42d50a50caaa25fad40ba8e5d3
Chance Rupnow <chancerupnow@gmail.com>
2022-10-20 18:22:12 -0400

    add budget button modal

--- src/components/BudgetContent.js
+++ src/components/BudgetContent.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Container, Row, Col, Card } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Container, Card, Row, Col, Image, Modal, Form, Button } from 'react-bootstrap'
 import Fab from '@mui/material/Fab';
 import { FaPlus } from 'react-icons/fa'
 import { PieChart, Pie, Cell, Legend } from 'recharts';
 
 export default function BudgetContent() {
+  const [open, setOpen] = useState(false);
+
+  // Modal close
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   // Sample data
   const dataGroc = [
     { name: 'Travel', value: 400 },
@@ -22,6 +29,41 @@ export default function BudgetContent() {
 
   return (
     <>
+      <Modal show={open} onClose={handleClose} onHide={handleClose}>
+        <Modal.Body>
+          <h2 className='text-center mb-4'>Add Budget</h2>
+          {/* {error && <Alert variant="danger">{error}</Alert>} */}
+          <Form>
+            <Row>
+              <Col sm={8}>
+                <Form.Group id='categories'>
+                  <Form.Label>Category</Form.Label>
+                  <Form.Control type='category'/>
+                  <Form.Label>Category</Form.Label>
+                  <Form.Control type='category'/>
+                  <Form.Label>Category</Form.Label>
+                  <Form.Control type='category'/>
+                </Form.Group>
+              </Col>
+              <Col sm={4}>
+                <Form.Group id='limit'>
+                  <Form.Label>Limit</Form.Label>
+                  <Form.Control type='value'/>
+                  <Form.Label>Limit</Form.Label>
+                  <Form.Control type='value'/>
+                  <Form.Label>Limit</Form.Label>
+                  <Form.Control type='value'/>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Button className='w-100 mt-3'>
+              Add
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+
+
       <Container style={{ width: '400px' }}>
         <PieChart width={400} height={250}>
           <Pie data={dataGroc} cx="50%" cy="50%" innerRadius={45} outerRadius={70} label>
@@ -66,7 +108,7 @@ export default function BudgetContent() {
       </Container>
 
       <Container style={{ position: "fixed", bottom: "20px", justifyContent: 'flex-end', display: 'flex' }}>
-        <Fab size={"80px"} color="primary" onClick={(e) => console.log('click')}>
+        <Fab size={"80px"} color="primary" onClick={(e) => setOpen(true)}>
           <FaPlus size={"30px"} />
         </Fab>
       </Container>
