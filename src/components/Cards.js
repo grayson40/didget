16b07854eea91f6c2ea160c4184173ffa2c779da
Jeanne Moore <mooresea910@gmail.com>
2022-10-21 12:45:58 -0400

    Changes to Dashboard cards so that the bar and pie charts can be switched out

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -20,16 +20,19 @@ export default function Cards() {
     // const [open2, setOpen2] = useState(true)
     const [open3, setOpen3] = useState(true);
     const [open4, setOpen4] = useState(true);
+    let budgetBool = true;
+    
 
   //    Use sample data for the different categories of bar graph
   const data = [
-    {name: 'Rent',          expense: 100, limit: 100},
-    {name: 'Groceries',     expense: 40, limit: 100},
-    {name: 'Food',          expense: 60, limit: 100},
-    {name: 'Insurance',     expense: 100,   limit: 100},
-    {name: 'Academic',      expense: 25, limit: 100},
-    {name: 'Entertainment', expense: 75, limit: 100}
+    {name: 'Rent',          value: 600.00,  expense: 100, limit: 600, max: 100},
+    {name: 'Groceries',     value: 138.25,   expense: 40,  limit: 200, max: 100},
+    {name: 'Food',          value: 45.65,   expense: 60,  limit: 100, max: 100},
+    {name: 'Insurance',     value: 200.00,  expense: 100, limit: 200, max: 100},
+    {name: 'Academic',      value: 18.99,   expense: 25,  limit: 60, max: 100},
+    {name: 'Entertainment', value: 75.00,   expense: 75,  limit: 100, max: 100}
   ];
+
 
   //    Use sample data for the different categories of pie chart
   const expenseData = [
@@ -91,7 +94,27 @@ export default function Cards() {
                 </Button>
                 <Collapse in={open3}>
                     <div class="container-fluid justify-content-center align-content-center" height={500}>
-                        {/* Create a pie chart to hold the data of each individual category */}
+                        {budgetBool == true ?
+                        /* Create a vertically aligned bar chart containing the dataset of limits and expense totals */
+                        <BarChart data={data} layout="vertical" width={560} height={250} >
+                            <Bar dataKey="expense" fill='#FFA07A' barSize={10}>
+                            {
+                                data.map((entry, index) => (
+                                <Cell key={'expense'} fill={expenseColors[index]}/>
+                                ))
+                            }
+                            </Bar>
+                            <Bar dataKey="max" barSize={10}>{
+                                data.map((entry, index) => (
+                                <Cell key={'max'} fill={limitColors[index]}/>
+                                ))}
+                            </Bar>
+                            <XAxis type="number" reversed />
+                            <YAxis type="category" width={150} padding={{ left: 20 }} dataKey="name" orientation="right" />
+                            <ReferenceLine x={100} stroke="red" strokeDasharray="3 3" />
+                        </BarChart>
+                        :
+                        /* Create a pie chart to hold the data of each individual category */
                         <PieChart width={400} height={250}>
                             <Pie data={expenseData} cx="50%" cy="50%" innerRadius={45} outerRadius={70}>
                             {
@@ -102,24 +125,8 @@ export default function Cards() {
                             </Pie>
                             <Legend layout='vertical' verticalAlign='middle' align='right'/>
                         </PieChart>
-                        {/* Create a vertically aligned bar chart containing the dataset of limits and expense totals */}
-                        <BarChart data={data} layout="vertical" width={400} height={250} >
-                            <Bar dataKey="expense" fill='#FFA07A' barSize={10}>
-                            {
-                                data.map((entry, index) => (
-                                <Cell key={'expense'} fill={expenseColors[index]}/>
-                                ))
-                            }
-                            </Bar>
-                            <Bar dataKey="limit" barSize={10}>{
-                                data.map((entry, index) => (
-                                <Cell key={'limit'} fill={limitColors[index]}/>
-                                ))}
-                            </Bar>
-                            <XAxis type="number" hide />
-                            <YAxis type="category" width={150} padding={{ left: 20 }} dataKey="name"/>
-                            <ReferenceLine x={100} stroke="red" strokeDasharray="3 3" />
-                        </BarChart>
+                        }
+                        
                         <Card style={{ width: '100%', textAlign: "Center" }} className="mb-2">
                             <Card.Body>
                                 <Row>
