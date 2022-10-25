307c410669c682bd406014623866863965ae58ac
Grayson Crozier <graysoncrozier40@gmail.com>
2022-10-25 12:34:33 -0400

    Update fetching, rendering of notes. Abstract state to parent

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,97 +1,21 @@
-import React, { useState, useEffect } from 'react'
-// import { Row, Col, Card, Button } from 'react-bootstrap';
-import {
-  List,
-  ListItem,
-  ListItemText,
-  Modal,
-} from '@material-ui/core';
-import { Button, Form, Card, Alert } from 'react-bootstrap';
-import { auth, db } from '../firebase'
-import { query, doc, collection, deleteDoc, getDocs, updateDoc } from 'firebase/firestore'
+import React, { useState, useRef } from 'react'
+import { Modal } from '@material-ui/core';
+import { Button, Form, Card, Row, Col } from 'react-bootstrap';
 import { FaTrashAlt, FaPen } from "react-icons/fa"
 
 export default function Note(props) {
   const [open, setOpen] = useState(false);
-  const [error, setError] = useState('')
-  const [input, setInput] = useState('')
-  const [note, setNote] = useState('')
+  const inputRef = useRef()
 
-  const deleteNote = async () => {
-    const usersRef = await getDocs(
-      query(
-        collection(db, 'users')
-      )
-    );
-    // Iterate through the documents fetched
-    usersRef.forEach(async (user) => {
-      if (user.data().uid === auth.currentUser.uid) {
-        const notesRef = await getDocs(
-          query(
-            collection(db, `users/${user.id}/notes`)
-          )
-        );
-        notesRef.forEach(async (note) => {
-          if (note.id === props.note.id) {
-            const docRef = doc(db, `users/${user.id}/notes/${note.id}`)
-            await deleteDoc(docRef)
-              .then(() => {
-                console.log('document deleted')
-                props.onUpdate()
-              })
-              .catch(error => {
-                setError(error.toString())
-              })
-          }
-        })
-      }
-    })
+  const handleUpdate = () => {
+    props.onUpdate(inputRef.current.value, props.note.noteId)
+    handleClose()
   }
-
-  // update note from firebase
-  const updateNote = async () => {
-    if (input !== '') {
-      const usersRef = await getDocs(
-        query(
-          collection(db, 'users')
-        )
-      );
-      // Iterate through the documents fetched
-      usersRef.forEach(async (user) => {
-        if (user.data().uid === auth.currentUser.uid) {
-          const notesRef = await getDocs(
-            query(
-              collection(db, `users/${user.id}/notes`)
-            )
-          );
-          notesRef.forEach(async (note) => {
-            if (note.id === props.note.id) {
-              const docRef = doc(db, `users/${user.id}/notes/${note.id}`)
-              await updateDoc(docRef, { note: input })
-                .then(() => {
-                  console.log('document updated')
-                  // props.onUpdate()
-                })
-                .catch(error => {
-                  setError(error.toString())
-                })
-            }
-          })
-        }
-      })
-      setNote(input)
-      setOpen(false);
-    }
-  };
 
   // Modal close
   const handleClose = () => {
     setOpen(false);
   };
-
-  useEffect(() => {
-    setNote(props.note.note);
-  }, [])
 
   return (
     <>
@@ -107,12 +31,12 @@ export default function Note(props) {
         }>
           <Card.Body>
             <h2 className='text-center mb-4'>Update Note</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
+            {/* {error && <Alert variant="danger">{error}</Alert>} */}
             <Form>
               <Form.Group id='note'>
-                <Form.Control type='note' onChange={(e) => setInput(e.target.value)} placeholder={note} />
+                <Form.Control type='note' ref={inputRef} placeholder={props.note.note} />
               </Form.Group>
-              <Button className='w-100 mt-3' onClick={updateNote}>
+              <Button className='w-100 mt-3' onClick={handleUpdate}>
                 Update
               </Button>
             </Form>
@@ -120,32 +44,42 @@ export default function Note(props) {
         </Card>
       </Modal>
 
-      <List>
-        <ListItem button>
-          <ListItemText primary={note} secondary={props.note.date} />
-          {props.inCard &&
-            <>
-              <Button
-                variant="contained"
-                color="primary"
-                size="medium"
-                onClick={(e) => setOpen(true)}
-              >
-                <FaPen />
-              </Button>
-              <span className="space"></span>
-              <Button
-                variant="contained"
-                color="secondary"
-                size="medium"
-                onClick={deleteNote}
-              >
-                <FaTrashAlt color='gray' />
-              </Button>
-            </>
-          }
-        </ListItem>
-      </List>
+      <Card className='mb-2'>
+        <Card.Body>
+          <Row>
+            <Col sm={8}>
+              <p>{props.note.note}</p>
+            </Col>
+            <Col sm={2}>
+              <p>{props.note.date}</p>
+            </Col>
+            {
+              props.inCard && 
+              <>
+                <Col sm={2}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                    onClick={(e) => setOpen(true)}
+                  >
+                    <FaPen />
+                  </Button>
+                  <span className="space"></span>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    size="medium"
+                    onClick={(e) => { props.onDelete(props.note.noteId) }}
+                  >
+                    <FaTrashAlt color='gray' />
+                  </Button>
+                </Col>
+              </>
+            }
+          </Row>
+        </Card.Body>
+      </Card>
     </>
   )
 }
