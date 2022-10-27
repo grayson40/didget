97a84fd00bbc8fadbf75aa86beac4c883be2e487
Grayson Crozier <graysoncrozier40@gmail.com>
2022-10-27 16:08:15 -0400

    Build budget functionality/backend architecture

--- src/components/BudgetContent.js
+++ src/components/BudgetContent.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react'
-import { Container, Card, Row, Col, Modal, Form, Button, ProgressBar } from 'react-bootstrap'
+import React, { useState, useRef, useEffect } from 'react'
+import { Container, Card, Row, Col, Modal, Form, Button } from 'react-bootstrap'
 import Fab from '@mui/material/Fab';
 import { FaPlus } from 'react-icons/fa'
 import { ReferenceLine, BarChart, Bar, Cell, XAxis, YAxis } from 'recharts';
+import {
+  collection,
+  getDocs,
+  query,
+  addDoc,
+  // doc,
+  // updateDoc,
+  // deleteDoc
+} from 'firebase/firestore';
+import { auth, db } from '../firebase';
+import BudgetItem from './BudgetItem';
 
 export default function BudgetContent() {
   const [open, setOpen] = useState(false);
+  const [budgets, setBudgets] = useState([]);
+  const rentLimit = useRef();
+  const groceriesLimit = useRef();
+  const foodLimit = useRef();
+  const insuranceLimit = useRef();
+  const academicLimit = useRef();
+  const entertainmentLimit = useRef();
+
   //    Use sample data for the different categories of bar graph
   const data = [
     { name: 'Rent', symbol: '🏠', value: 600.00, expense: 100, limit: 600, max: 100 },
@@ -35,18 +54,158 @@ export default function BudgetContent() {
     '#A569BD'
   ];
 
+  async function fetchData() {
+    if (auth.currentUser) {
+      const usersRef = await getDocs(
+        query(
+          collection(db, 'users')
+        )
+      );
+      // Iterate through the documents fetched
+      usersRef.forEach(async (user) => {
+        if (user.data().uid === auth.currentUser.uid) {
+          const categoriesRef = await getDocs(
+            query(
+              collection(db, `users/${user.id}/categories`)
+            )
+          );
+          setBudgets(
+            categoriesRef.docs.map((category) => ({
+              id: category.id,
+              category: category.data().category,
+              limit: category.data().limit,
+              current: category.data().current
+            }))
+          )
+        }
+      })
+    }
+    console.log('fetching budget data')
+  }
 
-  //Calulates the total amount of money left (spending limit - amount spent)
-  function left(limit, spent) {
-    return limit - spent
-  }
-  //Calculates the percentage for the progress bar ((total spent/spending limit)*100)
-  function progressPercent(limit, spent) {
-    return (spent / limit) * 100
-  }
+  // Used to fetch users notes from firestore
+  useEffect(() => {
+    fetchData();
+    console.log('in budget page effect')
+  }, [])
 
   const handleClose = () => {
     setOpen(false);
+  }
+
+  const updateBudget = () => {
+    // Update budget on screen
+    let updatedLimit = 0;
+    budgets.forEach((category) => {
+      switch (category.category) {
+        case "Rent":
+          updatedLimit = parseInt(rentLimit.current.value)
+          if (rentLimit.current.value !== '' && updatedLimit !== category.limit) {
+            category.limit = updatedLimit;
+          }
+          break;
+        case "Groceries":
+          updatedLimit = parseInt(groceriesLimit.current.value)
+          if (groceriesLimit.current.value !== '' && updatedLimit !== category.limit) {
+            category.limit = updatedLimit;
+          }
+          break;
+        case "Food":
+          updatedLimit = parseInt(foodLimit.current.value)
+          if (foodLimit.current.value !== '' && updatedLimit !== category.limit) {
+            category.limit = updatedLimit;
+          }
+          break;
+        case "Insurance":
+          updatedLimit = parseInt(insuranceLimit.current.value)
+          if (insuranceLimit.current.value !== '' && updatedLimit !== category.limit) {
+            category.limit = updatedLimit;
+          }
+          break;
+        case "Academic":
+          updatedLimit = parseInt(academicLimit.current.value)
+          if (academicLimit.current.value !== '' && updatedLimit !== category.limit) {
+            category.limit = updatedLimit;
+          }
+          break;
+        case "Entertainment":
+          updatedLimit = parseInt(entertainmentLimit.current.value)
+          if (groceriesLimit.current.value !== '' && updatedLimit !== category.limit) {
+            category.limit = updatedLimit;
+          }
+          break;
+        default:
+          break;
+      }
+    })
+
+    // TODO: update budget in db
+  }
+
+  const createBudget = () => {
+    // If category budget already set, update limits
+    // Else create budget
+    if (budgets.length !== 0) {
+      updateBudget()
+    } else {
+      // Add budgets to screen
+      const newBudgets = [
+        {
+          category: "Rent",
+          limit: parseInt(rentLimit.current.value),
+          current: 0
+        },
+        {
+          category: "Groceries",
+          limit: parseInt(groceriesLimit.current.value),
+          current: 0
+        },
+        {
+          category: "Food",
+          limit: parseInt(foodLimit.current.value),
+          current: 0
+        },
+        {
+          category: "Insurance",
+          limit: parseInt(insuranceLimit.current.value),
+          current: 0
+        },
+        {
+          category: "Academic",
+          limit: parseInt(academicLimit.current.value),
+          current: 0
+        },
+        {
+          category: "Entertainment",
+          limit: parseInt(entertainmentLimit.current.value),
+          current: 0
+        }
+      ]
+      setBudgets(newBudgets);
+
+      // Add budgets to db
+      newBudgets.forEach(async (category) => {
+        const newBudget = {
+          category: category.category,
+          limit: category.limit,
+          current: category.current
+        }
+        const userRef = await getDocs(
+          query(
+            collection(db, "users")
+          )
+        );
+        userRef.docs.map(async (user) => {
+          if (user.data().uid === auth.currentUser.uid) {
+            const collectionRef = collection(db, `users/${user.id}/categories/`);
+            await addDoc(collectionRef, newBudget);
+          }
+        })
+      })
+    }
+
+
+    handleClose()
   }
 
   return (
@@ -81,9 +240,57 @@ export default function BudgetContent() {
             <h2 className='text-center mb-4'>Create Budget</h2>
             {/* {error && <Alert variant="danger">{error}</Alert>} */}
             <Form>
-            {/* TODO: add budget category and limit fields */}
-              <Button className='w-100 mt-3' onClick={handleClose}>
-                Create
+              {/* TODO: add budget category and limit fields */}
+              <Form.Group id='rent'>
+                <Row className="mb-2">
+                  <Col className="border-end">Rent</Col>
+                  <Col>
+                    <Form.Control type='rent' ref={rentLimit} />
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Form.Group id='groceries'>
+                <Row className="mb-2">
+                  <Col className="border-end">Groceries</Col>
+                  <Col>
+                    <Form.Control type='groceries' ref={groceriesLimit} />
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Form.Group id='food'>
+                <Row className="mb-2">
+                  <Col className="border-end">Food</Col>
+                  <Col>
+                    <Form.Control type='food' ref={foodLimit} />
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Form.Group id='insurance'>
+                <Row className="mb-2">
+                  <Col className="border-end">Insurance</Col>
+                  <Col>
+                    <Form.Control type='insurance' ref={insuranceLimit} />
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Form.Group id='academic'>
+                <Row className="mb-2">
+                  <Col className="border-end">Academic</Col>
+                  <Col>
+                    <Form.Control type='academic' ref={academicLimit} />
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Form.Group id='entertainment'>
+                <Row className="mb-2">
+                  <Col className="border-end">Entertainment</Col>
+                  <Col>
+                    <Form.Control type='entertainment' ref={entertainmentLimit} />
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Button className='w-100 mt-3' onClick={createBudget}>
+                Create Budget
               </Button>
             </Form>
           </Modal.Body>
@@ -105,25 +312,9 @@ export default function BudgetContent() {
 
         {/*Cards with Name, Total, Category, and Date*/}
         {
-          data.map((item, index) => (
+          budgets.map((item, index) => (
             <>
-              <Card style={{ width: '500px', textAlign: "Center" }} className="mb-2">
-                <Card.Body>
-                  <Row className="mb-2">
-                    <Col className="border-end">{item.name}</Col>
-                    <Col>${item.value}</Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      {/*Compares current amount to limit, turns red if over and green if under limit*/}
-                      {left(item.limit, item.value) >= 0
-                        ? <ProgressBar variant="success" now={progressPercent(item.limit, item.value)} label={`$${item.value} Spent`} />
-                        : <ProgressBar variant="danger" now={progressPercent(item.limit, item.value)} label={`$${item.value} Spent`} />
-                      }
-                    </Col>
-                  </Row>
-                </Card.Body>
-              </Card>
+              <BudgetItem key={index} item={item}></BudgetItem>
             </>
           ))
         }
