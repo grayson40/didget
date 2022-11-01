7e0ce7f52f6365dace16a8ed6d20878870601f1d
Grayson Crozier <graysoncrozier40@gmail.com>
2022-11-01 15:53:50 -0400

    Fix task bug

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Card, Row, Col, Form, Button, Modal } from 'react-bootstrap'
 import { FaTrashAlt, FaPen } from 'react-icons/fa'
 
 export default function Task(props) {
-  const [name, setName] = useState('');
-  const [deadline, setDeadline] = useState('');
+  // const [name, setName] = useState('');
+  // const [deadline, setDeadline] = useState('');
   const [open, setOpen] = useState(false);
+  const nameRef = useRef();
+  const deadlineRef = useRef();
 
   useEffect(() => {
     console.log('in task effect')
-    setName(props.task.name);
-    setDeadline(props.task.deadline);
   }, [])
 
   const handleUpdate = () => {
-    props.onUpdate(props.task.id, name, deadline)
+    const _name = nameRef.current.value === '' ? props.task.name : nameRef.current.value
+    const _deadline = deadlineRef.current.value === '' ? props.task.deadline : deadlineRef.current.value
+    props.onUpdate(props.task.id, _name, _deadline)
     handleClose()
   }
 
@@ -32,19 +34,11 @@ export default function Task(props) {
           <Form>
             <Form.Group id='task'>
               <Form.Label>Task</Form.Label>
-              <Form.Control type='task' placeholder={name} onChange={(e) => {
-                  if (e.target.value !== '') {
-                    setName(e.target.value)
-                  }
-                }} />
+              <Form.Control type='task' placeholder={props.task.name} ref={nameRef} />
             </Form.Group>
             <Form.Group id='deadline'>
               <Form.Label>Deadline</Form.Label>
-              <Form.Control type='deadline' placeholder={deadline} onChange={(e) => {
-                  if (e.target.value !== '') {
-                    setDeadline(e.target.value)
-                  }
-                }}/>
+              <Form.Control type='deadline' placeholder={props.task.deadline} ref={deadlineRef}/>
             </Form.Group>
             <Button className='w-100 mt-3' onClick={handleUpdate}>
               Update
@@ -62,18 +56,18 @@ export default function Task(props) {
                 ? <>
                   <Col sm={1}><Form.Check defaultChecked onChange={(e) => {props.onCheck(props.task.id, props.task.isChecked)}} aria-label="option 1" /></Col>
                   <Col sm={5}>
-                    <p className="text-decoration-line-through" id={props.task.id}>{name}</p>
+                    <p className="text-decoration-line-through">{props.task.name}</p>
                   </Col>
                 </>
                 : <>
                   <Col sm={1}><Form.Check onChange={(e) => {props.onCheck(props.task.id, props.task.isChecked)}} aria-label="option 1" /></Col>
                   <Col sm={5}>
-                    <p id={props.task.id}>{props.task.name} {props.courseId}</p>
+                    <p>{props.task.name}</p>
                   </Col>
                 </>
             }
             <Col sm={3}>
-            <p id={props.task.id}>{deadline}</p>
+            <p id={props.task.id}>{props.task.deadline}</p>
             </Col>
             {
               props.showButtons &&
